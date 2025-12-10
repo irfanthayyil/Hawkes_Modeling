fdaefadc3e7364465fbd6c47a7a8bdaf59346f57
irfanthayyil <irfanthayyil@gmail.com>
2025-12-10 20:24:15 +0530

    clean folder

--- cat_results.py
+++
@@ -1,7 +0,0 @@
-def read_results():
-    try:
-        with open(r'data/outputs/hawkes_results.txt', 'r') as f:
-            print(f.read())
-    except Exception as e:
-        print(e)
-read_results()
--- read_results_temp.py
+++
@@ -1,8 +0,0 @@
-import os
-
-file_path = r'data/outputs/hawkes_results.txt'
-if os.path.exists(file_path):
-    with open(file_path, 'r') as f:
-        print(f.read())
-else:
-    print("File not found")
